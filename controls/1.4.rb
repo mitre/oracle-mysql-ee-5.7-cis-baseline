6f3199b3414df7f847695726805ff46f8550c32c
asturtevant <a.sturtevant4@gmail.com>
2019-04-02 10:26:42 -0400

    Update 1.4.rb

--- controls/1.4.rb
+++ controls/1.4.rb
@@ -5,6 +5,7 @@ control "1.4" do
   tag "severity": "medium"
   tag "cis_id": "1.4"
   tag "cis_level": 1
+  tag "nist": ['IA-5(5)', 'Rev_4']
   tag "Profile Applicability": "Level 1 - MySQL RDBMS on Linux"
   tag "audit text": "To assess this recommendation, use the /proc filesystem to determine if MYSQL_PWD is currently set for any process
   grep MYSQL_PWD /proc/*/environ
