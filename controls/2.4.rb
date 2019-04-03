5437c4ebf63288e4d0e0912b1c968953ce73b61c
HackerShark <melsharkawi@mitre.org>
2019-04-03 15:31:42 -0400

    added missing cis_level
    Signed-off-by: HackerShark <melsharkawi@mitre.org>

--- controls/2.4.rb
+++ controls/2.4.rb
@@ -1,11 +1,11 @@
 control "2.4" do
   title "Do Not Use Default or Shared Cryptographic Material (Not Scored)"
-  desc  "The cryptographic material used by MySQL, such as digital certificates and encryption keys, should be used only for MySQL and only for one instance. 
+  desc  "The cryptographic material used by MySQL, such as digital certificates and encryption keys, should be used only for MySQL and only for one instance.
   Default cryptographic material should not be used because others are likely to have copies of them"
   impact 0.5
   tag "severity": "medium"
   tag "cis_id": "2.4"
-  tag "cis_level": 
+  tag "cis_level": 2
   tag "nist": ['IA-5(2)', 'Rev_4']
   tag "Profile Applicability": "Level 2 - MySQL RDBMS on Linux"
   tag "audit text": "Review all cryptographic material and check to see if any of it is default or is used for other MySQL instances or for purposes other than MySQL"
