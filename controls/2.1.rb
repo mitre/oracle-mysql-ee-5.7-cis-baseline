b34e23f9204ad20b1cd43ae0814dcb1a24ecdb4f
Aaron Lippold <lippold@gmail.com>
2021-09-16 23:13:46 +0000

    fixed spelling in 2.1
    Signed-off-by: GitHub <noreply@github.com>

--- controls/2.1.rb
+++ controls/2.1.rb
@@ -9,7 +9,7 @@ control '2.1' do
   tag "Profile Applicability": 'Level 1 - MySQL RDBMS on Linux'
   tag "check": 'Verify there are no other roles enabled for the underlying operating system and that no additional applications or services unrelated to the proper operation of the MySQL server software are installed.'
   tag "fix": 'Remove excess applications or services and/or remove unnecessary roles from the underlying operating system'
-  describe 'A manual review is required to verfify a dedicated machine is running MySQL' do
-    skip 'A manual review is required to verfify a dedicated machine is running MySQL'
+  describe 'A manual review is required to verify a dedicated machine is running MySQL' do
+    skip 'A manual review is required to verify a dedicated machine is running MySQL'
   end
 end
